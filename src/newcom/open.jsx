c3dc0f68a63b653de0d0c99c5ae820750b39dbe1
Man Navlakha <mannavlakha1021@gmail.com>
2024-12-26 21:19:10 +0530

    bug

--- src/newcom/open.jsx
+++ src/newcom/open.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Document } from 'react-pdf'
 
 const open = () => {
   return (
@@ -7,6 +8,7 @@ const open = () => {
     <div className="bg-gray-200 p-4 rounded-lg shadow-inner">
         {Array(7).fill().map((_, index) => (
             <div key={index} className="bg-white p-4 rounded-full mb-4 shadow-md">
+                {/* <Document file="https://topperworld.in/media/2022/11/DSA-Handwritten-Notes.pdf" /> */}
                 Data Structure & Algorithm
             </div>
         ))}
