65484e5399a87f99f5ef4f4a787a15cb9bc0b8c7
Man Navlakha <mannavlakha1021@gmail.com>
2024-12-30 17:48:14 +0530

    Refactor routing structure in App component to remove deprecated Switch component and enhance readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './newcom/home'
-import Sub from './newcom/Sub'
-import Login from './newcom/Login'
-import Open from './newcom/open'
-import { Routes, Route, Link, Switch  } from 'react-router-dom'
-import Sign from './newcom/Sign'
-import Verify from './newcom/veri'
+import { useState } from 'react';
+import './App.css';
+import Home from './newcom/home';
+import Sub from './newcom/Sub';
+import Login from './newcom/Login';
+import Open from './newcom/open';
+import { Routes, Route } from 'react-router-dom';
+import Sign from './newcom/Sign';
+import Verify from './newcom/veri';
 import NotFound from './NotFound';
-import './index.css'
+import './index.css';
 
 function App() {
 
@@ -16,7 +16,6 @@ function App() {
     <div className="App">
 
       <Routes>
-        <Switch>
           <Route component={NotFound} />
           <Route path='/' element={<Home />}/>
           <Route path='/sub' element={<Sub />}/>
@@ -24,7 +23,6 @@ function App() {
           <Route path='/open' element={<Open />}/>
           <Route path='/signup' element={<Sign />}/>
           <Route path='/verification' element={<Verify />}/>
-        </Switch>
       </Routes>
     </div>
   )
