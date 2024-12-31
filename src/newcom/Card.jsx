42bf1a7472edc331c85cdd497710fbd7909b200e
Man Navlakha <mannavlakha1021@gmail.com>
2024-12-31 15:50:16 +0530

    Remove unused circle element from Card component

--- src/newcom/Card.jsx
+++ src/newcom/Card.jsx
@@ -10,7 +10,6 @@ const Card = (props) => {
   return (
     <div className="div h-[4em] w-32 hover:shadow-md hover:border-emerald-500 border-emerald-800 border-2 bg-white m-auto rounded-[1em] overflow-hidden relative group p-2 z-0">
       <div className="circle absolute h-[5em] w-[5em]  -top-[2.5em] border-emerald-800 border-2 blur-[5px] -right-[2.5em] rounded-full bg-emerald-200 group-hover:scale-[800%] duration-500 z-[-1] op" />
-      <div className="circle absolute h-[5em] w-[5em]  -bottom-[2.5em] border-emerald-800 border-2 blur-[15px] -left-[1.5em] rounded-full bg-emerald-100 group-hover:scale-[800%] duration-700 z-[-1] op" />
 
       <h1 className="z-20 text-green-900 font-bold font-Poppin group-hover:text-emerald-950 duration-500 text-[1.4em]">
         {props.b}
