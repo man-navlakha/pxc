f0c429f36595d9459cb3073da85dce88a79ccebf
Man Navlakha <mannavlakha1021@gmail.com>
2025-01-17 19:19:48 +0530

    Fix cookie setting for user ID in NewPassword component

--- src/newcom/newpassword.jsx
+++ src/newcom/newpassword.jsx
@@ -10,12 +10,12 @@ const NewPassword = () => {
   const [error, setError] = useState('');
   const navigate = useNavigate();
   const location = useLocation();
-  Cookies.set('user_id', user_id, { expires: 7 }); // Cookie expires in 7 days
-
+  
   // Get query parameters from URL
   const params = new URLSearchParams(location.search);
   // const userId = params.get("id"); // Use 'id' if that's what Django is sending
   const userId =  Cookies.get('user_id'); 
+  Cookies.set('user_id', user_id, { expires: 7 }); // Cookie expires in 7 days
               
   // Set a cookie
 
