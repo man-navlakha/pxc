ecfea9cf7a94726d5e4ae8baaed9a5c9fcac3984
Man Navlakha <mannavlakha1021@gmail.com>
2025-02-07 19:18:24 +0530

    Refactor Sem component styling and update Sub component layout for improved UI consistency

--- src/newcom/Sub.jsx
+++ src/newcom/Sub.jsx
@@ -21,8 +21,8 @@ const Sub = () => {
     </div>
     <p className="mt-4 text-left pl-3 text-lg">Select your subject</p>
 
-    <div className='z-1 rounded-t-lg mr-2 ml-2 mt-4' >
-        <div className='block shadow-[inset_1px_7px_4px_rgba(0,0,0,1)]  rounded-t-3xl'>
+    <div className='z-1 rounded-t-lg mr-2 ml-2 mt-4 bg-white' >
+        <div className='block shadow-[inset_0px_4px_4px_rgba(0,0,0)]  rounded-t-3xl'>
             <div className='-z-1 ml-2 p-4 mr-2 rounded-t-lg flex  overflow-x-scroll gap-6' >
                 <Sem s={1} />
                 <Sem s={2} />
@@ -35,16 +35,16 @@ const Sub = () => {
     
 
 
-            <div className="mt-6 space-y-4 p-4">
+            <div className="mt-6 space-y-4 p-4  h-dvh overflow-y-hidden">
                 <Link  to={`/ns?sub=datastructure`}>
-                <div className="flex items-center p-4 bg-gray-200 rounded-full shadow-md">
-                    <img src="https://placehold.co/50x50" alt="Icon for Data Structure & Algorithm" className="rounded-lg w-12 h-12 mr-4"/>
+                <div className="flex items-center p-4 bg-gray-200  shadow-md">
+                    <img src="https://thumbs.dreamstime.com/b/data-structures-algorithms-blue-gradient-concept-icon-problem-solving-programming-skill-abstract-idea-thin-line-illustration-248447079.jpg" alt="Icon for Data Structure & Algorithm" className="shadow-[3px_3px_0px_0px_#065f46] w-12 h-12 mr-4"/>
 
                     <span className="text-lg font-medium">Data Structure & Algorithm</span>
                 </div>
                     </Link>
-                <div className="flex items-center p-4 bg-gray-200 rounded-full shadow-md">
-                    <img src="https://placehold.co/50x50" alt="Icon for Data Structure & Algorithm" className="rounded-lg w-12 h-12 mr-4"/>
+                <div className="flex items-center p-4 bg-gray-200  shadow-md">
+                    <img src="https://thumbs.dreamstime.com/b/data-structures-algorithms-blue-gradient-concept-icon-problem-solving-programming-skill-abstract-idea-thin-line-illustration-248447079.jpg" alt="Icon for Data Structure & Algorithm" className="shadow-[3px_3px_0px_0px_#065f46] w-12 h-12 mr-4"/>
                     <span className="text-lg font-medium">Data Structure & Algorithm</span>
                 </div>
             
