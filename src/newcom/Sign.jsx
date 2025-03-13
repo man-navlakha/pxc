b0a0053c9501eb703f35c85c6eece77382505ab9
Man Navlakha <mannavlakha1021@gmail.com>
2025-03-13 15:08:13 +0530

    Update Sign component layout by adjusting margin and commenting out Footer

--- src/newcom/Sign.jsx
+++ src/newcom/Sign.jsx
@@ -149,7 +149,7 @@ const Sign = () => {
             </button>
           </div>
         </form>
-        <div className="mt-6">
+        <div className="mt-[76px]">
           {/* <button
             className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
           >
@@ -169,7 +169,7 @@ const Sign = () => {
           </div>
       </div>
     </div>
-      <Footer />
+      {/* <Footer /> */}
       </>
   );
 };
