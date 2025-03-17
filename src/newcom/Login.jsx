1950b73244f2bc3e5567b3f9c3f85fddf7b19caa
Man Navlakha <mannavlakha1021@gmail.com>
2025-03-17 18:17:06 +0530

    added password show in sign up page & fix in login

--- src/newcom/Login.jsx
+++ src/newcom/Login.jsx
@@ -100,7 +100,7 @@ const Login = () => {
             <input type={passwordVisible ? 'text' : 'password'} name="password" className="dark:text-gray-100 dark:bg-[#383838] mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required
               />
             <button className="-ml-8 drop-shadow drop-shadow-lg rounded-lg " type="button" onClick={togglePasswordVisibility} >
-              {passwordVisible ? '🔓' : '🔒'}
+              {passwordVisible ? '👁️' : '🤫'}
             </button>
               </div>
 
--- src/newcom/Sign.jsx
+++ src/newcom/Sign.jsx
@@ -15,7 +15,11 @@ const Sign = () => {
   const [course, setCourse] = useState('');
   const [loading, setLoading] = useState(false); // Define loading state
   const navigate = useNavigate(); // hook for redirecting
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
    useEffect(() => {
       
           axios.post("https://pixel-classes.onrender.com/api/home/courses", {})
@@ -129,25 +133,35 @@ const Sign = () => {
           </div>
           <div>
             <label className="block text-sm font-medium font-ff text-gray-700 dark:text-gray-100 ">Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter Password"
-              required
-              className="dark:text-gray-100 dark:bg-[#383838] mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-            />
+              <div class="flex item-center">
+              <input
+                type={passwordVisible ? 'text' : 'password'}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter Password"
+                required
+                className="dark:text-gray-100 dark:bg-[#383838] mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              />
+               <button className="-ml-8 drop-shadow drop-shadow-lg rounded-lg " type="button" onClick={togglePasswordVisibility} >
+              {passwordVisible ? '👁️' : '🤫'}
+            </button>
+            </div>
           </div>
           <div>
             <label className="block text-sm font-medium font-ff text-gray-700 dark:text-gray-100 ">Confirm Password</label>
+            <div class="flex item-center">
             <input
-              type="password"
+              type={passwordVisible ? 'text' : 'password'}
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
               placeholder="Enter Confirm Password"
               required
               className="dark:text-gray-100 dark:bg-[#383838] mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
             />
+             <button className="-ml-8 drop-shadow drop-shadow-lg rounded-lg " type="button" onClick={togglePasswordVisibility} >
+              {passwordVisible ? '👁️' : '🤫'}
+            </button>
+          </div>
           </div>
           <div>
   <label className="block text-sm font-medium font-ff text-gray-700 dark:text-gray-100">Select Your Course:</label>
