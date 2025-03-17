1ef580345bd3b3033c9c67b2df1f42b84a5d1476
Man Navlakha <mannavlakha1021@gmail.com>
2025-03-17 18:58:06 +0530

    added/removed  fontawesome edited eye emoji

---
+++ src/componets/Ceye.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import eye from '../assets/svg/eye-slash-solid.svg'
+
+
+const Ceye = () => {
+  return (
+    <div>
+      <img src={eye} alt="Hide" />
+    </div>
+  )
+}
+
+export default Ceye
--- src/newcom/Login.jsx
+++ src/newcom/Login.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { useNavigate, Link } from 'react-router-dom';
 import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Oeye from "../componets/Oeye";
+import Ceye from "../componets/Ceye";
 
 const Login = () => {
   const [error, setError] = useState('');
@@ -100,7 +103,7 @@ const Login = () => {
             <input type={passwordVisible ? 'text' : 'password'} name="password" className="dark:text-gray-100 dark:bg-[#383838] mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required
               />
             <button className="-ml-8 drop-shadow drop-shadow-lg rounded-lg " type="button" onClick={togglePasswordVisibility} >
-              {passwordVisible ? '👁️' : '🤫'}
+              {passwordVisible ? '🔒' : '👁️'}
             </button>
               </div>
 
