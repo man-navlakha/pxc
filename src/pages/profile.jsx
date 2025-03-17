d6df3794468d1d3043aaad04ff674a1bf2b6f242
Man Navlakha <mannavlakha1021@gmail.com>
2025-03-17 16:47:51 +0530

    updated profile page & fix the issue in NS page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -8,10 +8,15 @@ import { useNavigate } from 'react-router-dom'; // Make sure to import useNaviga
 import { useEffect } from 'react'; // Import useEffect
 
 const username = Cookies.get('username') || 'John Doe';
-
 const Profile = () => {
-    const navigate = useNavigate(); // Initialize navigate
-
+  const navigate = useNavigate(); // Initialize navigate
+  
+  const handleLogoutClick = () => {
+    navigate('/logout');
+  };
+  const handleUpdateClick = () => {
+    alert("Coming Soon");
+  };
     useEffect(() => {
       const token = Cookies.get('access_token');
       if (token) {
@@ -26,7 +31,7 @@ const Profile = () => {
         <>
         <GoBack />
         <div className="flex flex-col items-center p-6  ">
-            <div className="bg-white dark:bg-[#383838] shadow-md rounded-lg p-6 w-full max-w-md">
+            <div className="bg-[#e9e9e9] dark:bg-[#383838] shadow-md rounded-lg p-6 w-full max-w-md">
                 <div className="flex flex-col items-center">
                     <img src="https://ik.imagekit.io/pxc/def.jpg" alt="Profile" className="w-32 h-32 rounded-full mb-4" />
                     <h1 className="text-2xl font-bold mb-2">{username}</h1>
@@ -35,6 +40,10 @@ const Profile = () => {
                 </div>
                 <div className="profile-content">
                     {/* Add your profile content here */}
+                    <div className='flex justify-between m-3 p-4'>
+                      <div className='bg-red-500 px-6 py-2 rounded-xl'onClick={handleLogoutClick} >Logout</div>
+                      <div className='bg-emerald-500 px-6 py-2 rounded-xl' onClick={handleUpdateClick}>Update Profile</div>
+                    </div >
                 </div>
             </div>
         </div>
