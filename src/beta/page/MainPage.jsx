61ca4313d0935aa0be157d8535ed97c5211795b3
Man Navlakha <mannavlakha1021@gmail.com>
2025-06-09 23:36:15 +0530

    footer & login fix design fix

--- src/beta/page/MainPage.jsx
+++ src/beta/page/MainPage.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import Navbar from '../componet/Navbar'
+import React, { useState } from 'react'
+import Navbar from '../componet/navbar'
 import Hero from '../componet/Hero'
 import '../new.css'
+import Cookies from "js-cookie";
 import Feature from '../componet/feature'
 import AskJavaQuestion from '../../componets/AskJavaQuestion'
+import Footer from '../componet/Footer';
 
 const MainPage = () => {
+
+          const token = Cookies.get("access_token");
+        
+
   return (
     <>
 
       <div className="bg-pattern "></div>
       <div className='geist h-screen text-white overflow-scroll flex flex-col text-center content-center flex-nowrap  gap-3  ' style={{ background: "radial-gradient(circle, rgb(10, 121, 45), #000)" }}>
         <Navbar />
-
+{
+  token ? '' : <>
         <Hero />
-        <Feature />
+        {/* <Feature /> */}
+  </>
+  
+}
       </div>
+      <Footer/>
+
     </>
   )
 }
