65cdf013f94e00c4e93c562180a0081dc32c7a7d
Man Navlakha <mannavlakha1021@gmail.com>
2025-07-23 16:51:36 +0530

    404 added

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -3,16 +3,14 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-red-600">Coming Soon</h1>
-      <p className="mt-4 text-xl text-gray-700">Page Soon </p>
-      <p className="mt-2 text-gray-500">Sorry, the page you are looking for does not exist.</p>
-      <Link 
-        to="/" 
-        className="mt-6 px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition duration-200"
-      >
-        Go back to Home
-      </Link>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
+       <div class="backdrop-blur-[7.2px] bg-white/0 border border-white/35 rounded-2xl shadow-[0_4px_30px_rgba(0,0,0,0.1)] p-10 text-center text-white max-w-md">
+    <h1 class="text-6xl font-bold">404</h1>
+    <p class="text-xl mt-4">Sorry, the page you’re looking for doesn’t exist.</p>
+    <a href="/" class="inline-block mt-6 px-6 py-3 border border-white/50 rounded-xl transition duration-300 hover:bg-white/20">
+      Back to Home
+    </a>
+  </div>
     </div>
   );
 };
