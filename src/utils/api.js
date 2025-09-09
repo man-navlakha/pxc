1f3171fb82113646e863b55089990fe17fae5f83
man-navlakha <dhruvsharma56780@gmail.com>
2025-09-09 14:15:02 +0530

    chabges in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api", // local fallback
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   xsrfCookieName: "csrftoken",
   xsrfHeaderName: "X-CSRFToken",
